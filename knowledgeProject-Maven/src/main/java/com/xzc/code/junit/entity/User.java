6c3d2813c7529c65f30a541e8c5f3ae153f04899
zhichao.xiao <yemuxiakeda@163.com>
2017-05-03 13:39:54 +0800

    junit实体

---
+++ knowledgeProject-Maven/src/main/java/com/xzc/code/junit/entity/User.java
@@ -0,0 +1,33 @@
+package com.xzc.code.junit.entity;
+
+public class User {
+	private Long sid;
+	private String name;
+    private int sex;
+    private int age;
+	public Long getSid() {
+		return sid;
+	}
+	public void setSid(Long sid) {
+		this.sid = sid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+    
+}
