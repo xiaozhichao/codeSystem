1b43755e588e795ca2914cea1dc8751f7ac3119e
xiaozhichao <yemuxiakeda@163.com>
2017-08-11 15:07:40 +0800

    配置

--- knowledgeProject-Maven/src/main/java/com/xzc/code/json/JsonTest.java
+++ knowledgeProject-Maven/src/main/java/com/xzc/code/json/JsonTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 public class JsonTest {
@@ -16,25 +17,30 @@ public class JsonTest {
 		bean.setDetail(null);
 		System.out.println();
 		JSONObject json = JSONObject.parseObject(JSONObject.toJSON(bean).toString());
-		
+		System.out.println(json);
 		List<JSONBeanDetail> details = new ArrayList<JSONBeanDetail>();
 		JSONBeanDetail detail1 = new JSONBeanDetail();
 		detail1.setId(11);
-		detail1.setName("张三详情");
+		detail1.setName("[{\"id\":\"1\"}]");
 		
 		JSONBeanDetail detail2 = new JSONBeanDetail();
 		detail2.setId(12);
-		detail2.setName("张三详情");
+		detail2.setName("[{\"id\":\"1\"}]");
 		
 		details.add(detail1);
 		details.add(detail2);
 		
-		json.put("details", details);
+		json.put("details", JSONArray.toJSON(details));
 		System.out.println(json);
 		
-		
-		
-		
+		JSONArray a = new JSONArray();
+		for(JSONBeanDetail detail:details){
+			String name = detail.getName();
+			JSONObject json1 = JSONObject.parseObject(JSONObject.toJSON(detail).toString());
+			json1.put("name", JSONObject.parse(name));
+			a.add(json1);
+		}
+		System.out.println(a);
 		
 		
 	}
